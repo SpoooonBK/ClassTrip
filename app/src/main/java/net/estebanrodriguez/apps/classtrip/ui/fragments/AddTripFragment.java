cc0e3ed20cb9d11db55b73c61f0ff410a99c9ba8
SpoooonBK <erodriguez927@gmail.com>
2017-07-16 18:46:29 -0400

    Added Date Picker Dialog in addTrip dateEditText

--- app/src/main/java/net/estebanrodriguez/apps/classtrip/ui/fragments/AddTripFragment.java
+++ app/src/main/java/net/estebanrodriguez/apps/classtrip/ui/fragments/AddTripFragment.java
@@ -1,15 +1,21 @@
 package net.estebanrodriguez.apps.classtrip.ui.fragments;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import net.estebanrodriguez.apps.classtrip.R;
 import net.estebanrodriguez.apps.classtrip.model.trip.Trip;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,14 +29,49 @@ public class AddTripFragment extends Fragment{
     @BindView(R.id.add_trip_name_edit_text) EditText nameEditText;
     @BindView(R.id.add_trip_address_edit_text) EditText addressEditText;
 
+    final Calendar calendar = Calendar.getInstance();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.add_trip, container, false);
         ButterKnife.bind(this, rootView);
         Timber.v("add trip");
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getActivity(),
+                        date,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DATE))
+                        .show();
+
+            }
+        });
+
         return rootView;
     }
+
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateEditText.setText(sdf.format(calendar.getTime()));
+    }
+
 
     @OnClick(R.id.add_trip_done_fab)
     public void onAddTrip(){
@@ -46,6 +87,9 @@ public class AddTripFragment extends Fragment{
                 .withPlace(address)
                 .build();
     }
+
+
+
 
     @Override
     public void onResume() {
